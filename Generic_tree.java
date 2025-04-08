afeb190af55b8e93f91e8f72bbe6656a026b7391
Harshit Kumar <kharshit242@gmail.com>
2025-04-08 07:18:06 +0530

    Basic Structure Generic tree

---
+++ Generic_tree.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Generic_tree{
+    private class Node{
+        int data;
+        ArrayList<Node>children=new ArrayList<>();
+    }
+    public static void main(String args[]){
+        
+    }
+}
