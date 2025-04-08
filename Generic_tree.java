5fcc14d0edb1e046ee27673d1bfbb3cfca7c4c80
Harshit Kumar <kharshit242@gmail.com>
2025-04-08 07:51:01 +0530

    Display Tree

--- Generic_tree.java
+++ Generic_tree.java
@@ -5,8 +5,22 @@ public class Generic_tree{
         int data;
         ArrayList<Node>children=new ArrayList<>();
     }
+
+    public static void display(Node node){
+        String str=node.data+"->";
+
+        for(Node child:node.children){
+            str+=child.data+",";
+        }
+        str+=".";
+        System.out.println(str);
+
+        for(Node child:node.children){
+            display(child);
+        }
+    }
     public static void main(String args[]){
-        Node root;
+        Node root=new Node();
         int[] arr={10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
 
         Stack<Node> st=new Stack<>();
@@ -28,5 +42,7 @@ public class Generic_tree{
                 st.push(t);
             }
         }
+
+        display(root);
     }
 }
